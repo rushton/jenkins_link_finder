2c76259182d3f8979689441187d11c40fb41f4e3
Nick Rushton <nick@hasoffers.com>
2015-03-19 15:02:06 -0700

    add colors and font size

--- content.js
+++ content.js
@@ -1,26 +1,29 @@
 jQuery.noConflict();
 jQuery(document).ready(function() {
-        var lines = jQuery('pre.console-output').text().split('\n');
-        var links = lines.map(function(v,k) {
-            if(v.indexOf("Test failed: ") > 0) {
-                test_name = v.split('Test failed: ')[1];
-                search_offset = 1;
-                while(true) {
-                    if(lines[k - search_offset].indexOf("Executing: GET") > 0) {
-                        return {'test': test_name, 'url': lines[k-search_offset].split('GET')[1]};
-                    }
-                    search_offset++;
-                    if (search_offset > 100) {
-                        console.log("couldn't find url for " + test_names[idx])
-                        return "";
-                    }
+    var lines = jQuery('pre.console-output').text().split('\n');
+    var links = lines.map(function(v,k) {
+        if(v.indexOf("Test failed: ") > 0) {
+            test_name = v.split('Test failed: ')[1];
+            search_offset = 1;
+            while(true) {
+                if(lines[k - search_offset].indexOf("Executing: GET") > 0) {
+                    return {'test': test_name, 'url': lines[k-search_offset].split('GET')[1]};
+                }
+                search_offset++;
+                if (search_offset > 100) {
+                    console.log("couldn't find url for " + test_names[idx])
+                    return "";
                 }
             }
-            return "";
-        }).filter(function(v) { return v != "" });
-        console.log("FAILING URLS");
+        }
+        return "";
+    }).filter(function(v) { return v != "" });
+    if (links.length > 0) {
+        console.log("%cREGRESSION FAILURES", 'font-size:15px;color:#a00');
         links.forEach(function(v) {
            console.log(v.test + ": " + v.url);
         });
-
+    } else {
+        console.log("%cREGRESSION SUCCESS, NO FAILURES.", 'font-size:15px;color:#0a0');
+    }
 });
